92fe4bb15e5943d10cf2a1d8efc26f6aaa689fef
Smallbai <1140743786@qq.com>
2019-07-02 11:22:29 +0800

    提交测试21

--- Text1/src/main/java/Hello.java
+++ Text1/src/main/java/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Hello IDEA");
+        int index = 0;
+        System.out.println();
+        String[] str = {"1","3",3+"5",5+"2","s4","s6"};
+        for (String s: str) {
+            System.out.println(s);
+        }
     }
 }
